1e133fad30928a43846f72d88e61a22e348a5e65
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-09-27 13:13:44 +0200

    commit paquetes para ordenar

--- app/src/main/java/com/example/firestoredemo/vista/MainActivity.java
+++ app/src/main/java/com/example/firestoredemo/vista/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.firestoredemo.vista;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.firestoredemo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,10 +41,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+
         inicioSesion = findViewById(R.id.inicioSesion);
         registrarboton = findViewById(R.id.btn_registro);
         modoInvitado = findViewById(R.id.btn_inicioInvitado);
-
+        // esto mira si ya estaba logeado anteriormente
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             currentUser.reload();
@@ -60,7 +69,24 @@ public class MainActivity extends AppCompatActivity {
                 contra = passwLogin.getText().toString();
 
                 if (!(gmail.isEmpty()) && !(contra.isEmpty())) {
+                    mAuth.signInWithEmailAndPassword(gmail, contra)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
 
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        updateUI(null);
+                                    }
+                                }
+                            });
 
                 } else {
                     Toast.makeText(getApplicationContext(), "Rellene con los datos necesarios para iniciar sesión.", Toast.LENGTH_SHORT).show();

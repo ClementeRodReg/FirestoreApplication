3d1c58a06d4cd4245a33f010687499db30deca01
Sergio <sergio.galerafr@elorrieta-errekamari.com>
2023-10-02 12:33:03 +0200

    EventoSeleccionado ventana en proceso

--- app/src/main/java/com/example/firestoredemo/vista/EventoSeleccionado.java
+++ app/src/main/java/com/example/firestoredemo/vista/EventoSeleccionado.java
@@ -2,29 +2,23 @@ package com.example.firestoredemo.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.firestoredemo.R;
 import com.example.firestoredemo.metodos.MetodosEventoSeleccionado;
+
+import java.util.ArrayList;
 
 
 public class EventoSeleccionado extends AppCompatActivity {
 
     TextView lblEventoSeleccionado;
     ImageView imageView;
-    TextView editTextDia;
-    TextView editTextMes;
-    TextView editTextAno;
-    Button btnContinuar;
-    String dia, mes, ano;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,45 +29,17 @@ public class EventoSeleccionado extends AppCompatActivity {
 
         int imagenReferencia = getIntent().getIntExtra("clave_datoImagen", 0);
 
+        // Iniciadores de ID
+
         lblEventoSeleccionado = findViewById(R.id.lblEventoSeleccionado);
         imageView = findViewById(R.id.fotoSeleccionada);
-        editTextDia = findViewById(R.id.editTextDia);
-        editTextMes = findViewById(R.id.editTextMes);
-        editTextAno = findViewById(R.id.editTextAno);
-        btnContinuar = findViewById(R.id.btnContinuar);
 
         imageView.setImageResource(imagenReferencia);
 
         String nombreEvento = getIntent().getStringExtra("clave_datoNombre");
         lblEventoSeleccionado.setText(nombreEvento.toString());
 
-        metodosEventoSeleccionado.limitadorDeEscritura2(editTextDia);
-        metodosEventoSeleccionado.limitadorDeEscritura2(editTextMes);
-        metodosEventoSeleccionado.limitadorDeEscritura4(editTextAno);
 
-        btnContinuar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                dia = editTextDia.getText().toString();
-                mes = editTextMes.getText().toString();
-                ano = editTextAno.getText().toString();
-
-                if(!(dia.isEmpty()) && !(mes.isEmpty()) && !(ano.isEmpty())){
-
-                    if(dia.length() == 2 && mes.length() == 2 && ano.length() == 4){
-
-                        String fecha = editTextDia.getText().toString() + "/" + editTextMes.getText().toString() + "/" + editTextAno.getText().toString();
-                        Toast.makeText(EventoSeleccionado.this, "La fecha es " + fecha.toString(), Toast.LENGTH_SHORT).show();
-
-                    }else{
-                        Toast.makeText(EventoSeleccionado.this, "Compruebe que ha rellenado bien todos los campos", Toast.LENGTH_SHORT).show();
-                    }
-                }else{
-                    Toast.makeText(EventoSeleccionado.this, "Debe rellenar todos los campos", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
     }
 }

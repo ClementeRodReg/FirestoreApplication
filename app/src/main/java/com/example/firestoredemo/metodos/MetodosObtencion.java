f4dce7f746102fc352a9f689aa04d74f93cfbf8b
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-10-11 13:16:30 +0200

    commit sacar datos con handler para sincronizar

--- app/src/main/java/com/example/firestoredemo/metodos/MetodosObtencion.java
+++ app/src/main/java/com/example/firestoredemo/metodos/MetodosObtencion.java
@@ -2,6 +2,7 @@ package com.example.firestoredemo.metodos;
 
 
 import com.example.firestoredemo.modelo.Obras;
+import com.example.firestoredemo.modelo.Salas;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -46,11 +47,13 @@ public class MetodosObtencion {
         return obrasList;
     }
 
-    public ArrayList<String> obtenerEdificios(String obra){
-        ArrayList<String> edificios = new ArrayList<>();
+    public ArrayList<Salas> obtenerEdificios(String obra){
 
+        ArrayList<Salas> edificios = new ArrayList<>();
         myBBDD = FirebaseFirestore.getInstance();
         Task coleccion = myBBDD.collection("Salas").get();
+
+        ArrayList<Salas> edificiosEnLosQueSeCelebra = new ArrayList<>();
 
         coleccion.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -59,7 +62,18 @@ public class MetodosObtencion {
                     // Bucle de los edificios que tiene la coleccion que se manda
                     for (QueryDocumentSnapshot documentLocal : task.getResult()) {
                         //Sacar el HashMap de Firebase
-                        edificios.add(documentLocal.getId());
+                        ArrayList<String> nombreSalas = new ArrayList<>();
+
+                        //bucle para sacar sus datos
+                        for (Map.Entry<String, Object> pair : documentLocal.getData().entrySet()) {
+                            nombreSalas.add(pair.getValue().toString());
+                        }
+
+                        //insertamos todo en la calse personalizada
+                        Salas edifio = new Salas(documentLocal.getId(), nombreSalas);
+
+                        //metemos el objeto personalizado en el Arraylist
+                        edificios.add(edifio);
                     }
                 }
             }
@@ -67,7 +81,36 @@ public class MetodosObtencion {
 
         Task coleccion2 = myBBDD.collection("SeCelebraT").get();
 
-        return edificios;
+        coleccion2.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task2) {
+                if (task2.isSuccessful()) {
+                    // Bucle de los edificios que tiene la coleccion que se manda
+                    for (QueryDocumentSnapshot FechaSala : task2.getResult()) {
+                        //dividir el id que recibimos de firebase
+                        String fecha = FechaSala.getId().split("_")[0];
+                        String sala = FechaSala.getId().split("_")[1];
+                        if(!edificiosEnLosQueSeCelebra.contains(sala)){
+
+                            if(FechaSala.get("NombreObra").equals(obra)){
+
+                                System.out.println(sala);
+                            }
+
+                        }
+
+
+
+                    }
+                }
+            }
+        });
+
+
+
+
+
+        return edificiosEnLosQueSeCelebra;
     }
 
 }

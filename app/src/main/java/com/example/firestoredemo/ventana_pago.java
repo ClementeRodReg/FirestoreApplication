c2c761ec8247160b37aecfab64af88e092cccf8f
Sergio <sergio.galerafr@elorrieta-errekamari.com>
2023-10-18 12:52:08 +0200

    ventana_pago

---
+++ app/src/main/java/com/example/firestoredemo/ventana_pago.java
@@ -0,0 +1,14 @@
+package com.example.firestoredemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana_pago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_pago);
+    }
+}

4da43f9cc71c34c759d54be433bc165892b11adf
Clemen <clemente.rodriguezre@elorrieta-errekamari.com>
2023-10-20 13:02:27 +0200

    commit metodo borrar tickets y borrar ticket individual

---
+++ app/src/main/java/com/example/firestoredemo/metodos/MetodoBorrarTickets.java
@@ -0,0 +1,71 @@
+package com.example.firestoredemo.metodos;
+
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class MetodoBorrarTickets {
+    FirebaseFirestore myBBDD;
+
+    public void borrarTickets() {
+
+        myBBDD = FirebaseFirestore.getInstance();
+        Task coleccion = myBBDD.collection("Tickets").get();
+        coleccion.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    for (QueryDocumentSnapshot documentoTiecket : task.getResult()) {
+
+                        myBBDD.collection("Tickets").document(documentoTiecket.getId())
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "Ticket eliminado!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error deleting document", e);
+                                    }
+                                });
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    public void borrarTicket(String nomTicket) {
+
+        myBBDD = FirebaseFirestore.getInstance();
+        Task coleccion = myBBDD.collection("Tickets").document(nomTicket).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully deleted!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error deleting document", e);
+                    }
+                });
+
+    }
+
+}
